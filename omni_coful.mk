5831e5e6bc110483c0064aa570167cc1b4d94c21
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2023-02-25 00:12:45 +0530

    Update omni_coful.mk
    Testing if its booting or not.

--- omni_coful.mk
+++ omni_coful.mk
@@ -5,18 +5,34 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Device Target Name
+PRODUCT_RELEASE_NAME := coful
 
 # Inherit from coful device
 $(call inherit-product, device/motorola/coful/device.mk)
 
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Virtual A/B OTA
+# https://source.android.com/docs/core/ota/virtual_ab/implement#build-flags
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk
+
+# Enable updating of APEXes
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Device Identifier
 PRODUCT_DEVICE := coful
-PRODUCT_NAME := omni_coful
+PRODUCT_NAME := twrp_coful
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g31
 PRODUCT_MANUFACTURER := motorola
